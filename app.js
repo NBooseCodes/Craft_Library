b755b109b0c7c57b48abcd70296f4e327d593641
NBooseCodes <nmboose@gmail.com>
2024-07-27 17:54:50 -0700

    Added handlebars

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ var app = express();
 PORT = 21210
 var db = require('./database/db-connection')
 
+// Plug in to Handlebars
+
+const {engine} = require('express-handlebars');
+var exphbs = require('express-handlebars'); // Import handlebars
+app.engine('.hbs', engine({extname: ".hbs"})); // Create handlebars instance on which we can do work
+app.set('view engine', '.hbs'); // Tell express to use handlebars when it encounters a .hbs file
+
 // Routes
 app.get('/', function(req, res){
-    res.send('Server is up and running');
+    res.render('home');
 })
 
 // Listender
